ba1549da2843bb05c5cff6084f1ee2271a31f2bc
Divy Viradiya <divyviradiya2@gmail.com>
2025-12-13 17:56:21 +0530

    links updated

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,12 +1,12 @@
 export const LINKS = {
   zomato: 'https://www.zomato.com/surat/blue-can-coffee-4-utran/order?v=o2',
   swiggy: 'https://www.swiggy.com/city/surat/blue-can-coffee-sarthana-surat-rest1048515',
-  instagram: 'https://instagram.com/bluecancoffee',
-  maps: 'https://www.google.com/maps/place/Blue+Can+Coffee/@21.2302,72.8567,17z',
-  mapsEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.5!2d72.8567!3d21.2302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sBlue%20Can%20Coffee!5e0!3m2!1sen!2sin!4v1234567890',
+  instagram: 'https://instagram.com/bluecancoffee_india',
+  maps: 'https://maps.app.goo.gl/t3QacxVpvJZLPbebA',
+  mapsEmbed: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d29751.639952117886!2d72.8625877!3d21.2336332!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04f6f9e4aef77%3A0xe34a95fea3468162!2sBlue%20Can%20Coffee!5e0!3m2!1sen!2sin!4v1765628612828!5m2!1sen!2sin',
   phone: '+918238550081',
   address: 'Blue Can Coffee, Uttran, Surat, Gujarat 394105',
-  googleReviews: 'https://www.google.com/maps/place/Blue+Can+Coffee/@21.2302,72.8567,17z/data=!4m8!3m7!1s0x0:0x0!8m2!3d21.2302!4d72.8567!9m1!1b1!16s',
+  googleReviews: 'https://www.google.com/maps/place/Blue+Can+Coffee/@21.2336332,72.8625877,14z/data=!4m15!1m8!3m7!1s0x3be04f6f9e4aef77:0xe34a95fea3468162!2sBlue+Can+Coffee!8m2!3d21.2337811!4d72.8627198!10e1!16s%2Fg%2F11rmynk2y4!3m5!1s0x3be04f6f9e4aef77:0xe34a95fea3468162!8m2!3d21.2337811!4d72.8627198!16s%2Fg%2F11rmynk2y4?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D',
 } as const;
 
 export const BUSINESS_HOURS = {
